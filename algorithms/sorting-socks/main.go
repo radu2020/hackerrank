bbf4cc7ed7fb3b712f87648269d31ae9fcac0c87
Radu <radu@MB-Pro.fritz.box>
2020-10-14 21:41:33 +0200

    add socks algorithms

---
+++ algorithms/sorting-socks/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// Complete the sockMerchant function below.
+func sockMerchant(ar []int32) int32 {
+
+	myMap := make(map[int32]int32)
+	for i:=0; i<len(ar)-1; i++{
+		if _, ok := myMap[ar[i]]; ok {
+			continue
+		} else {
+			myMap[ar[i]] = 1
+		}
+		for j:=i+1; j<len(ar); j++ {
+			if ar[i] == ar[j] {
+				val := myMap[ar[i]]
+				myMap[ar[i]] = val + 1
+			}
+		}
+	}
+	var totalPairs int32 = 0
+	for _, v := range myMap {
+		if v > 1 {
+			res := v / 2
+			totalPairs += res
+		}
+	}
+	return totalPairs
+}
+
+func main() {
+	list:= []int32{10, 20, 20, 10, 10, 30, 50, 10, 20}
+	//list_2:= []int32{1, 1, 3, 1, 2, 1, 3, 3, 3, 3}
+	ret := sockMerchant(list)
+	fmt.Print("RET IS...", ret, "\n")
+}
